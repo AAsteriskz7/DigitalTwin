14a42fd553dce3d436e62e63dea060ce5f758754
Sandro <sandroqarqusashvili@gmail.com>
2025-04-06 00:45:29 -0400

    Revert "animated text"
    This reverts commit 991bdcdc128925e4f7540a8fffe4ef2fdf93e3b0.

--- frontend/src/HomePage.js
+++ frontend/src/HomePage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Typewriter } from 'react-simple-typewriter';
 import './HomePage.css';
 
 function HomePage() {
@@ -13,26 +12,10 @@ function HomePage() {
       <div className="blur blur3"></div>
       <div className="blur blur4"></div>
 
-      <h1 className="title">
-        Digital T
-        <span className="animated-twin">
-        <Typewriter
-  words={['T', '', 'win']}
-  loop={1}
-  cursor
-  cursorStyle="_"
-  typeSpeed={120}      // ↓ from 200
-  deleteSpeed={80}     // ↓ from 100
-  delaySpeed={400}     // ↓ from 1000
-/>
-
-        </span>
-      </h1>
-
+      <h1 className="title">DigitalTwin</h1>
       <h4 className="subtitle">
-        AI-driven health insights, personalized for you.
-        <br />
-        Step into the future of proactive care, powered by intelligent technology.
+      AI-driven health insights, personalized for you.
+       <br /> Step into the future of proactive care, powered by intelligent technology.
       </h4>
 
       <button className="demo-btn" onClick={() => navigate('/dataone')}>
@@ -43,3 +26,4 @@ function HomePage() {
 }
 
 export default HomePage;
+
